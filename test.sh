b3d3e233ff024795c6891849c42fb8cdf18bcb96
Chris Pressey <cpressey@catseye.tc>
2021-06-27 17:35:55 +0100

    Run unit tests under Python 2, or 3, or both 2 and 3 if both present.

--- test.sh
+++ test.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
-PYTHONPATH=src python2 src/samovar/tests.py || exit 1
-PYTHONPATH=src python3 src/samovar/tests.py || exit 1
+if [ "x$PYTHON" != "x" ]; then
+    if command -v "$PYTHON" > /dev/null 2>&1; then
+        PYTHONPATH=src $PYTHON src/samovar/tests.py || exit 1
+    else
+        echo "$PYTHON not found on executable search path. Aborting."
+        exit 1
+    fi
+else
+    MISSING=""
+    if command -v python2 > /dev/null 2>&1; then
+        PYTHONPATH=src python2 src/samovar/tests.py || exit 1
+    else
+        MISSING="${MISSING}2"
+    fi
+    if command -v python3 > /dev/null 2>&1; then
+        PYTHONPATH=src python3 src/samovar/tests.py || exit 1
+    else
+        MISSING="${MISSING}3"
+    fi
+    if [ "x${MISSING}" = "x23" ]; then
+        echo "Neither python2 nor python3 found on executable search path. Aborting."
+        exit 1
+    fi
+fi
+
 falderal doc/Samovar.md || exit 1
