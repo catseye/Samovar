47e72227f6c4c5f01ec769dd9ab9459db77710ee
Chris Pressey <cpressey@catseye.tc>
2022-10-19 14:20:11 +0100

    Use "but only if shell command succeeds", ditch test appliances.

--- doc/appliances/samovar.py2.md
+++
@@ -1,4 +0,0 @@
-    -> Functionality "Run Samovar Simulation" is implemented by
-    -> shell command
-    -> "python2 bin/samovar %(test-body-file) --min-events 4 --randomness-type canned"
-    -> but only if shell command "command -v python2" is successful
--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-APPLIANCES=""
-
 if [ "x$PYTHON" != "x" ]; then
     if command -v "$PYTHON" > /dev/null 2>&1; then
         PYTHONPATH=src $PYTHON src/samovar/tests.py || exit 1
@@ -13,13 +11,11 @@ else
     MISSING=""
     if command -v python2 > /dev/null 2>&1; then
         PYTHONPATH=src python2 src/samovar/tests.py || exit 1
-        APPLIANCES="$APPLIANCES doc/appliances/samovar.py2.md"
     else
         MISSING="${MISSING}2"
     fi
     if command -v python3 > /dev/null 2>&1; then
         PYTHONPATH=src python3 src/samovar/tests.py || exit 1
-        APPLIANCES="$APPLIANCES doc/appliances/samovar.py3.md"
     else
         MISSING="${MISSING}3"
     fi
@@ -29,4 +25,4 @@ else
     fi
 fi
 
-falderal $APPLIANCES doc/Samovar.md || exit 1
+falderal --verbose doc/Samovar.md || exit 1
